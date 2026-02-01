9bd3febdc8a1d1987e20fd49d32009e6b58fa8e5
jakebaynie23-creator <jakebaynie23@gmail.com>
2026-02-01 16:22:42 +1100

    Update Success.js

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,26 +1,18 @@
-import React from "react";
-import kisses from "../kisses.gif";
+import "../App.css";
 
-/**
- * Success component displaying a sweet message upon acceptance.
- *
- * @returns {JSX.Element} JSX element representing the Success component.
- */
-const Success = () => (
-  <div className="App-success">
-    {/* Displaying a cute kisses gif */}
-    <img className="App-gif" src={kisses} alt="Kisses" />
-    {/* Personalized success messages */}
-    <p className="App-text-success">
-      You're my favorite movie buddy, my princess, and the one I enjoy every
-      moment with.
-    </p>
-    <p className="App-text-success">
-      Looking forward to spending Valentine's Day with you, Jocelyn.
-    </p>
-    {/* Displaying the date of the special moment */}
-    <p className="App-text-date">12/23/2023</p>
-  </div>
-);
+function Success() {
+  return (
+    <div className="container">
+      <h1>Yay! 😸✨🎉</h1>
+
+      <img
+        src="https://media.giphy.com/media/TSpM3iivfaVfH5zjAC/giphy.gif"
+        alt="Celebration gif"
+        style={{ maxWidth: "300px", marginTop: "20px" }}
+      />
+    </div>
+  );
+}
 
 export default Success;
+
