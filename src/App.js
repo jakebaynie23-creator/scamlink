427916b412761751381e914023b382c56e89979f
jakebaynie23-creator <jakebaynie23@gmail.com>
2026-02-01 16:28:25 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,18 @@
-import React, { useState } from "react";
-import "./App.css";
-import Success from "./components/Success";
+import { useState } from "react";
 import Asking from "./components/Asking";
-import flowerBear from "./flowerBear.gif";
-import madBear from "./madBear.gif";
+import Success from "./components/Success";
+import "./App.css";
 
-/**
- * Main App component managing the Valentine's Day proposal.
- *
- * @returns {JSX.Element} JSX element representing the App component.
- */
-const App = () => {
-  // State to track acceptance and rejection
-  const [accepted, setAccepted] = useState(false);
-  const [rejected, setRejected] = useState(false);
-  const [noButtonText, setNoButtonText] = useState("No");
-  const [lastRejectedIndex, setLastRejectedIndex] = useState(-1);
-
-  // Handler for accepting the proposal
-  const handleAccept = () => {
-    setAccepted(true);
-  };
-
-  // Handler for rejecting the proposal
-  const handleReject = () => {
-    setRejected(true);
-    // Array of rejection messages
-    const rejectionTexts = [
-      "Are you sure?",
-      "Maybe try again?",
-      "Think again!",
-      "Loser :(",
-    ];
-    // Randomly select a rejection message
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * rejectionTexts.length);
-    } while (randomIndex === lastRejectedIndex);
-
-    setLastRejectedIndex(randomIndex);
-    setNoButtonText(rejectionTexts[randomIndex]);
-  };
+export default function App() {
+  const [answeredYes, setAnsweredYes] = useState(false);
 
   return (
     <div className="App">
-      <div className="App-body">
-        {/* Asking to be my Valentine */}
-        {!accepted && (
-          <Asking
-            gif={rejected ? madBear : flowerBear}
-            altText={rejected ? "Rejected Bear" : "I love you Bear"}
-            handleAccept={handleAccept}
-            handleReject={handleReject}
-            noButtonText={noButtonText}
-          />
-        )}
-
-        {/* She said YES! */}
-        {accepted && <Success />}
-      </div>
+      {answeredYes ? (
+        <Success />
+      ) : (
+        <Asking onYes={() => setAnsweredYes(true)} />
+      )}
     </div>
   );
-};
-
-export default App;
+}
